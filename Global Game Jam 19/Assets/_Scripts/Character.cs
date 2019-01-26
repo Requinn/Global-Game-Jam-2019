1b8972ef97b55b0a11e0fe8e02ec6937f44bbb33
Joel Lee <lee.joel395@gmail.com>
2019-01-26 00:30:01 -0800

    Protoype Character Controller, Layer additions

---
+++ Global Game Jam 19/Assets/_Scripts/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base character class
+/// </summary>
+public class Character : MonoBehaviour
+{
+    private CharacterMotor _motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _motor = GetComponent<CharacterMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //handle inputs here
+        _motor.Move(Input.GetAxis("Horizontal"));
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+            _motor.Jump();
+        }
+    }
+}
