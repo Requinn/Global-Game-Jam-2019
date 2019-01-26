590053302b305af67227529ee02495e364befd64
Paper-Hat <justinkristian@gmail.com>
2019-01-26 11:59:43 -0800

    Props

---
+++ Global Game Jam 19/Assets/_PropScripting/Spring.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    [SerializeField]
+    private float launch;
+    private Coroutine springJuice;
+    private Animator springAnimator;
+    private int springHash;
+    
+    private void Start()
+    {
+        springAnimator = GetComponentInChildren<Animator>();
+        springHash = Animator.StringToHash("Active");
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.attachedRigidbody)
+            Launch(other.attachedRigidbody);
+    }
+
+    private void Launch(Rigidbody2D arb2d)
+    {
+        arb2d.velocity = new Vector2(arb2d.velocity.x, 0);
+        arb2d.AddForce(new Vector2(0, launch), ForceMode2D.Force);
+        Debug.Log("Launched.");
+        springJuice = StartCoroutine(ISpringJuice());
+        
+    }
+
+    IEnumerator ISpringJuice()
+    {
+        Debug.Log("Started.");
+        springAnimator.SetTrigger(springHash);
+        yield return null;
+        springJuice = null;
+    }
+
+    void FixedUpdate()
+    {
+        if(springJuice == null)
+            springAnimator.ResetTrigger(springHash);
+    }
+    
+
+}
