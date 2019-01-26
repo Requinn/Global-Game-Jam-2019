30e26291b6f60a14e28fb8642c99ef666e8f02de
Paper-Hat <justinkristian@gmail.com>
2019-01-26 15:57:37 -0800

    MovingPlatform Complete

---
+++ Global Game Jam 19/Assets/_Scripts/_PropScripting/PlatformParenting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformParenting : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.gameObject.transform.parent = transform;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        other.gameObject.transform.parent = null;
+    }
+}
