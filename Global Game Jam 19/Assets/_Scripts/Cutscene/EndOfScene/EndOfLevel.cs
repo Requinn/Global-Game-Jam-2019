250cd32bd84a7f87a4aec11358caac93de990c2f
Joel Lee <lee.joel395@gmail.com>
2019-01-27 14:43:41 -0800

    End of level cutscene prefab

--- Global Game Jam 19/Assets/_Scripts/Cutscene/EndOfScene/EndOfLevel.cs
+++ Global Game Jam 19/Assets/_Scripts/Cutscene/EndOfScene/EndOfLevel.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class EndOfLevel : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+/// <summary>
+/// End of level sequence. 
+/// </summary>
+public class EndOfLevel : SequenceObject {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField]
+    private SceneLoader _sceneLoader;
+
+    public override IEnumerator DoSequenceAction() {
+        float time = 0;
+        while (time < 1f) {
+            time += Time.deltaTime;
+            _affectingPlayer.DoMovements(.75f, false);
+            yield return null;
+        }
+        _sceneLoader.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        yield return null;
     }
 }
