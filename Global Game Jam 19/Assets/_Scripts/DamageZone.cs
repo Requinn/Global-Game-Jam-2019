29edd775c12b249009971c15f9e13198aeb6d4a9
Michaelwolf95 <michaelwolf95@gmail.com>
2019-01-27 16:11:35 -0800

    SFX and level 2

--- Global Game Jam 19/Assets/_Scripts/DamageZone.cs
+++ Global Game Jam 19/Assets/_Scripts/DamageZone.cs
@@ -5,9 +5,14 @@ public class DamageZone : MonoBehaviour
 {
     public int damage = 1;
 
+    public AudioSource audioSource;
+    public AudioClip deathSound;
+
     public void OnTriggerEnter2D(Collider2D c) {
         if (c.CompareTag("Player")) {
             c.GetComponent<Character>().Reset();
+            if (audioSource)
+                audioSource.PlayOneShot(deathSound);
         }
     }
 }
